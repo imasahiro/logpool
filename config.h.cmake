ae097986d5df1047360863416585e95aae9a8284
Masahiro Ide <imasahiro@users.sourceforge.jp>
2012-01-30 19:56:38 +0900

    fix llvm configure in CMakeLists.txt

--- config.h.cmake
+++ config.h.cmake
@@ -1,8 +1,6 @@
 #ifndef LOGPOOL_CONFIG_H_
 #define LOGPOOL_CONFIG_H_
 
-#cmakedefine HAVE_LIBDL        ${HAVE_LIBDL}
-#cmakedefine HAVE_LIBM         ${HAVE_LIBM}
 #cmakedefine HAVE_LIBMEMCACHED ${HAVE_LIBMEMCACHED}
 #cmakedefine SIZEOF_VOIDP  ${SIZEOF_VOIDP}
 #cmakedefine SIZEOF_LONG   ${SIZEOF_LONG}
