a0186b04debd184dfe55b62f90c82ba09118b5c9
Masahiro Ide <imasahiro@users.sourceforge.jp>
2012-05-25 19:07:16 +0900

    update tests

--- lio/test/test_q.c
+++ lio/test/test_q.c
@@ -3,11 +3,12 @@
 
 int main(int argc, char const* argv[])
 {
-    int i, j;
-    struct query_entry e;
+    int i;
+    //struct query_entry e;
     for (i = 0; i < 10000; ++i) {
         struct query_list *l = query_new();
         //TODO
+        //int j;
         //for (j = 0; j < 10000; ++j) {
         //    query_add(l, NULL, &e);
         //}
